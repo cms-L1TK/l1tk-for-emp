db687adddeda643d8012c211fcc0a7f3e94b9781
Mei <mei-li.holmberg@cern.ch>
2024-04-29 14:57:12 +0100

    added constraints files and updated dr vsim.tcl

--- dr/firmware/ucf/vsim.tcl
+++ dr/firmware/ucf/vsim.tcl
@@ -1,3 +1,10 @@
 set_property SOURCE_SET sources_1 [get_filesets sim_1]
 add_files -fileset sim_1 -norecurse ../emData/in.txt
 add_files -fileset sim_1 -norecurse ../emData/out.txt
+add_files -fileset constrs_1 -norecurse constraints.xdc
+
+set_property -name {STEPS.SYNTH_DESIGN.ARGS.MORE OPTIONS} -value {-mode out_of_context} -objects [get_runs synth_1]
+set_property STEPS.SYNTH_DESIGN.ARGS.FLATTEN_HIERARCHY none [get_runs synth_1] 
+set_property STEPS.SYNTH_DESIGN.ARGS.KEEP_EQUIVALENT_REGISTERS true [get_runs synth_1]
+set_top emp_payload
+update_compile_order -fileset sources_1
