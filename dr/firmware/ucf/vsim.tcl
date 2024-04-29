49ca7e5ae8a526f6112fe5a56f604cfb04f7019b
Mei <mei-li.holmberg@cern.ch>
2024-04-29 15:31:04 +0100

    fix emp crash and 360 MHz

--- dr/firmware/ucf/vsim.tcl
+++ dr/firmware/ucf/vsim.tcl
@@ -1,7 +1,7 @@
 set_property SOURCE_SET sources_1 [get_filesets sim_1]
 add_files -fileset sim_1 -norecurse ../emData/in.txt
 add_files -fileset sim_1 -norecurse ../emData/out.txt
-add_files -fileset constrs_1 -norecurse constraints.xdc
+add_files -fileset constrs_1 -norecurse ../../src/l1tk-for-emp/dr/firmware/ucf/constraints.xdc
 
 set_property -name {STEPS.SYNTH_DESIGN.ARGS.MORE OPTIONS} -value {-mode out_of_context} -objects [get_runs synth_1]
 set_property STEPS.SYNTH_DESIGN.ARGS.FLATTEN_HIERARCHY none [get_runs synth_1] 
